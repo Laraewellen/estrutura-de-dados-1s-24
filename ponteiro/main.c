e369906f886ca8efac0e1fa917c64123a6b9374f
laraewellen <laraewellen@gmail.com>
2024-04-09 10:21:15 -0300

    commit

--- ponteiro/main.c
+++ ponteiro/main.c
@@ -7,8 +7,10 @@ int main(int argc, char* argv[]){
     int b = a;
 
     printf("a = %d \t b = %d\n", a, b);
-
     printf("&a = %p \t &b = %p\n", &a, &b);
+
+    b = 3;
+    printf(" a= %d b = %d\n", a, b);
 
     exit (0);
 }
