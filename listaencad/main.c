927de256328810b29b6fb6a4158933841150f5b4
laraewellen <laraewellen@gmail.com>
2024-04-16 11:02:42 -0300

    add

---
+++ listaencad/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista.h"
+
+int main(int argc, char* argv[]){
+
+No* H = no('A', no('B', no('C', no('E', no('F', NULL)))));
+
+imprimir_lista(H);
+
+printf("\n qtd ="%d")
+
+    exit(0);
+}
