b739db51a6de678456283612ec5b47a5b5282029
Lara Ewellen <laraewellen@gmail.com>
2024-06-06 20:16:36 -0300

    commit

--- tabela_hash/tabela_hash.h
+++ tabela_hash/tabela_hash.h
@@ -3,8 +3,8 @@
 #define TAM_VALOR 255
 
 int hash(char* chave);
-
 char** tabela_hash();
 void put(char* chave, char* valor, char** tabela);
 char* get(char* chave, char** tabela);
-void remove(char*chave, char** tabela);
+int contains(char* chave, char** tabela);
+void remover(char* chave, char** tabela); 
